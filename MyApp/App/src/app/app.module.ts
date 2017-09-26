ff6517619a329b7694c58b48d06d7a2350032e71
fableo <fabioleotta@yahoo.it>
2017-09-26 12:27:18 +0200

    Directive

--- MyApp/App/src/app/app.module.ts
+++ MyApp/App/src/app/app.module.ts
@@ -2,15 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CarouselComponent } from './carousel/carousel.component';
-import { CarouselItemComponent } from './carousel-item/carousel-item.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarouselComponent,
-    CarouselItemComponent
   ],
   imports: [
     BrowserModule
--- MyApp/App/src/app/carousel.ts
+++
@@ -1,8 +0,0 @@
-export class Carousel {
-    src: string;
-    delay: number;
-    constructor(src: string, delay: number) {
-        this.src = src;
-        this.delay = delay;
-    }
-}
