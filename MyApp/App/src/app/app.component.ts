61b6c32f442209aa2616dc141920a217b3793b23
fableo <fabioleotta@yahoo.it>
2017-10-17 18:21:19 +0200

    directive

--- MyApp/App/src/app/app.component.ts
+++ MyApp/App/src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  elements: any[] = [
+    {
+      'type':  'text',
+      'value': 'Il mio primo testo',
+      'title': 'Il mio primo titolo'
+    },
+    {
+      'type':  'text',
+      'value': 'Il mio secondo testo',
+      'title': 'Il mio secondo titolo'
+    },
+    {
+      'type':  'img',
+      'value': 'https://dummyimage.com/600x400/000/fff',
+      'title': 'Il mio terzo titolo'
+    },
+    {
+      'type':  'img',
+      'value': 'https://dummyimage.com/600x400/ccc/0011ff',
+      'title': 'Il mio quarto titolo'
+    }
+  ];
 }
