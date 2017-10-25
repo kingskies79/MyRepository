78045148ef138d9865b56034d0c97484b39c2b95
fableo <fabioleotta@yahoo.it>
2017-10-25 15:47:42 +0200

    garage

--- skyline/src/app/veicolo/veicolo.component.ts
+++ skyline/src/app/veicolo/veicolo.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { Veicolo } from '../veicolo';
 @Component({
   selector: 'app-veicolo',
   templateUrl: './veicolo.component.html',
   styleUrls: ['./veicolo.component.css']
 })
-export class VeicoloComponent implements OnInit {
-
+export class VeicoloComponent  implements OnInit {
+  @Input() mezzi: Veicolo;
   constructor() { }
 
   ngOnInit() {
