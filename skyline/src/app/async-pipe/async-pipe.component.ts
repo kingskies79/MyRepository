ff3f4dc81d0d88170cc42da8750ed7aedeaf3224
fableo <fabioleotta@yahoo.it>
2017-10-27 13:48:40 +0200

    Observable Promise Observable data

--- skyline/src/app/async-pipe/async-pipe.component.ts
+++ skyline/src/app/async-pipe/async-pipe.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {Observable} from 'rxjs/RX';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { Observable, Subscriber, Subscription } from 'rxjs/RX';
 
 @Component({
   selector: 'app-async-pipe',
@@ -8,27 +8,38 @@ import {Observable} from 'rxjs/RX';
   styleUrls: ['./async-pipe.component.css']
 })
 export class AsyncPipeComponent implements OnInit, OnDestroy {
+  promise: Promise<string>;
+  observable: Observable<number>;
+  subscription: Subscription;
   observableData: number;
-  subscription: Object ;
 
   constructor() {
+    this.promise = this.getPromise();
+    this.observable = this.getObservable();
+
     this.subscribeObservable();
+
   }
   getObservable() {
     return Observable
-          .interval(1000)
-          .take(10)
-          .map ((v) => v * v);
+      .interval(1000)
+      .take(10)
+      .map((v) => v * v);
   }
-    subscribeObservable() {
-      this.subscription = this.getObservable()
-      .subscribe( v => this.observableData = v);
-    }
-
+  subscribeObservable() {
+    this.subscription = this.getObservable()
+      .subscribe(v => this.observableData = v);
+  }
+  getPromise() {
+    return new Promise((resolve, reject) => { setTimeout(() => resolve('Promise Complete!'), 3000); });
+  }
   ngOnInit() {
   }
   ngOnDestroy() {
-
+    if (this.subscribeObservable) {
+      this.subscription.unsubscribe();
+      console.log('subscription.unsubscribe');
+    }
 
   }
 
