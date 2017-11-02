80ba3250a68e5e5c44262d0dc11df8e27fb967ad
fableo <fabioleotta@yahoo.it>
2017-11-02 12:59:42 +0100

    D I P

--- skyline/src/app/app.component.ts
+++ skyline/src/app/app.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
-import {ReflectiveInjector, OpaqueToken} from '@angular/core';
-import {MandrillService} from './mandrill-service';
-import {SendGridService} from './send-grid-service';
-import {EmailService} from './email-service';
-import {PhoneService} from './phone-service';
+
+
 
 @Component({
   selector: 'app-root',
@@ -12,30 +9,4 @@ import {PhoneService} from './phone-service';
 })
 export class AppComponent {
 
-
-   constructor() {
-    const injector = ReflectiveInjector.resolveAndCreate([MandrillService, SendGridService]);
-    const emailService = injector.get(MandrillService);
-    console.log(emailService);
-     console.log('Injector Caching Example');
-     const emailService1 = injector.get(MandrillService);
-     const emailService2 = injector.get(MandrillService);
-     console.log(emailService1 === emailService2);
-     console.log('Injector Caching State Sharing Example');
-     emailService1.foo = 'moo';
-     console.log(emailService2.foo);
-     console.log('Chield Injector Forwards Request to Parent');
-     const injector1 = ReflectiveInjector.resolveAndCreate([EmailService]);
-     const childInjector = injector1.resolveAndCreateChild([]);
-     console.log(injector1.get(EmailService) === childInjector.get(EmailService));
-     console.log('Chield Injector Returns Diferent Instance');
-     const injector2 = ReflectiveInjector.resolveAndCreate([EmailService]);
-     const childInjector2 = injector2.resolveAndCreateChild([EmailService]);
-     console.log(injector2.get(EmailService) === childInjector2.get(EmailService));
-
-
-   }
-
-
-    }
-
+}
--- skyline/src/app/app.module.ts
+++ skyline/src/app/app.module.ts
@@ -2,16 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {FormGroup, FormControl, ReactiveFormsModule, FormsModule, Validator, FormBuilder} from '@angular/forms';
+import { SimpleComponent } from './simple/simple.component';
+import { OtherService } from './other-service';
+import { SimpleService } from './simple-service';
+import { ParentComponent } from './parent/parent.component';
+import { ChildComponent } from './child/child.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SimpleComponent,
+    ParentComponent,
+    ChildComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule
   ],
-  providers: [],
+  providers: [SimpleService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
