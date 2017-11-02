dfe81b2081c0680fa7e9385f611b01160d4c636b
fableo <fabioleotta@yahoo.it>
2017-11-02 14:53:25 +0100

    Joke Provider Store

--- skyline/src/app/app.module.ts
+++ skyline/src/app/app.module.ts
@@ -2,24 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {FormGroup, FormControl, ReactiveFormsModule, FormsModule, Validator, FormBuilder} from '@angular/forms';
-import { SimpleComponent } from './simple/simple.component';
-import { OtherService } from './other-service';
-import { SimpleService } from './simple-service';
-import { ParentComponent } from './parent/parent.component';
-import { ChildComponent } from './child/child.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    SimpleComponent,
-    ParentComponent,
-    ChildComponent
+
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule
   ],
-  providers: [SimpleService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
