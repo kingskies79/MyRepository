cdc8fe76bb22770704fdca10911e743561f044f9
fableo <fabioleotta@yahoo.it>
2017-11-04 13:30:23 +0100

    http

--- skyline/src/app/app.component.ts
+++ skyline/src/app/app.component.ts
@@ -1,10 +1,74 @@
 import { Component } from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/RX';
+import {URLSearchParams, Http, Response, RequestOptions, Headers, HttpModule} from '@angular/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+
 })
 export class AppComponent {
-  title = 'Skyline';
+
+  apiRoot = 'http://httpbin.org';
+  constructor (private http: Http) {
+
+  }
+  doGet () {
+    console.log('GET');
+    const url = `${this.apiRoot}/get`;
+    const search = new URLSearchParams();
+    search.set('foo', 'moo') ;
+    search.set('limit', '25');
+    this.http.get(url, {search: search}).subscribe(res => console.log(res.json()));
+    // this.http.get(url).subscribe(res => console.log(res.text()));
+    // this.http.get(url).subscribe(res => console.log(res.json()));
+  }
+  doPost() {
+    console.log('POST');
+    const url = `${this.apiRoot}/post`;
+    const search = new URLSearchParams();
+    this.http.post(url, {moo: 'foo', goo: 'loo'}).subscribe(res => console.log(res.json()));
+  }
+  doPut() {
+    console.log('PUT');
+  }
+  doDelete() {
+    console.log('DELETE');
+    const url = `${this.apiRoot}/delete`;
+    const search = new URLSearchParams();
+    search.set('foo', 'moo');
+    search.set('limit', '25');
+    this.http.delete(url, {search: search}).subscribe(res => console.log(res.json()));
+  }
+  doGETAsPromise() {
+    console.log('Get as Promise');
+    const url = `${this.apiRoot}/get`;
+    this.http.get(url)
+    .toPromise()
+    .then(res => console.log(res.json()));
+  }
+  doGETAsPromiseError() {
+    console.log('Get as Promise Error');
+    const url = `${this.apiRoot}/post`;
+    this.http.get(url)
+    .toPromise()
+    .then(res => console.log(res.json()), msg => console.error (`Error: ${msg.status} ${msg.statusText}`));
+  }
+  doGETAsObservableError() {
+    console.log('Get as Observable Error');
+    console.log('Get as Promise Error');
+    const url = `${this.apiRoot}/post`;
+    this.http.get(url).subscribe(res => console.log(res.json()), msg => console.error(`Error: ${msg.status} ${msg.statusText}`));
+  }
+  doGETWithHeaders() {
+    console.log('Get with Headers');
+    const headers: Headers = new Headers();
+    headers.append('Authorization', btoa('username:password'));
+    const opts: RequestOptions = new RequestOptions();
+    opts.headers = headers;
+    const url = `${this.apiRoot}/get`;
+    this.http.get(url, opts).subscribe(res => console.log(res.json()), msg => console.error (`Error: ${msg.status} ${msg.statusText}`));
+  }
 }
--- skyline/src/app/app.module.ts
+++ skyline/src/app/app.module.ts
@@ -1,25 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, OpaqueToken, Inject } from '@angular/core';
-
+import {Http, Response, RequestOptions, Headers, HttpModule} from '@angular/http';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { JokeComponent } from './joke/joke.component';
-import { JokeListComponent } from './joke-list/joke-list.component';
-import { FormComponent } from './form/form.component';
-import {JokeService} from './joke-service';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    JokeComponent,
-    JokeListComponent,
-    FormComponent
+    AppComponent
+
   ],
 
-  imports: [BrowserModule],
+  imports: [BrowserModule, HttpModule],
 
-  providers: [JokeService],
+  providers: [],
 
   bootstrap: [AppComponent]
 })
