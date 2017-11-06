543d6cba17033fefc6bdd33eb56f021d5138f898
fableo <fabioleotta@yahoo.it>
2017-11-06 15:43:43 +0100

    http promise

---
+++ skyline/src/app/search-item.ts
@@ -0,0 +1,3 @@
+export class SearchItem {
+    constructor(public track: string, public artist: string, public link: string, public thumbnail: string,  public artistId: string) {}
+}
---
+++ skyline/src/app/search-service.ts
@@ -0,0 +1,35 @@
+import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
+import {SearchItem} from './search-item';
+import 'rxjs/RX';
+ @Injectable()
+export class SearchService {
+    apiRoot = 'https://itunes.apple.com/search';
+    results: Object[];
+    loading: boolean;
+        constructor(private http: Http) {
+            this.results = [];
+            this.loading = false;
+
+    }
+    search(term: string) {
+        const promise = new Promise((resolve, reject) => {
+            const apiURL = `${this.apiRoot}?term=${term}&media=music&limit=20`;
+            this.http.get(apiURL)
+            .toPromise()
+            .then(
+                res => {this.results = res.json().results
+                    .map(item => {return new SearchItem
+                        (item.trackName, item.artistName, item.trackViewUrl, item.artworkUrl30, item.artistId); });
+                    console.log(this.results);
+                     resolve(res);
+
+            },
+            msg => {
+                  reject(msg);
+            }
+            );
+        });
+         return promise;
+    }
+}
