56210038f7ac7578e34791f03439d41888fea67a
kingskies79 <kingskies@gmail.com>
2018-04-04 11:58:55 +0200

    fdg

--- skyline/src/app/app.component.ts
+++ skyline/src/app/app.component.ts
@@ -1,25 +1,62 @@
 import { Component, OnInit } from '@angular/core';
-
-
-
+import { AuthService } from './providers/auth.service';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection
+} from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+export interface Todo {
+  id?: string;
+  description: string;
+  completed: boolean;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
+  email: string;
+  password: string;
+  todoCollectionRef: AngularFirestoreCollection<Todo>;
+  todo$: Observable<Todo[]>;
 
 
+  constructor(private afs: AngularFirestore, public authService: AuthService) {
+  this.todoCollectionRef = afs.collection<Todo>('todos');
+  this.todo$ = this.todoCollectionRef.snapshotChanges().map(actions => {
+    return actions.map(action => {
+      const data = action.payload.doc.data() as Todo;
+      const id = action.payload.doc.id;
+      return { id, ...data };
+    });
+  });
+}
+updateTodo(todo: Todo) {
 
-
-
-  constructor () {
-
+  this.todoCollectionRef.doc(todo.id).update({ completed: !todo.completed });
+}
+addTodo(todoDesc: string) {
+  console.log(todoDesc);
+  if (todoDesc && todoDesc.trim().length) {
+    this.todoCollectionRef.add({ description: todoDesc, completed: false });
+  }
+}
+deleteTodo(todo: Todo) {
+  this.todoCollectionRef.doc(todo.id).delete();
+}
+  signup() {
+    this.authService.signup(this.email, this.password);
+    this.email = this.password = '';
   }
 
+  login() {
+    this.authService.login(this.email, this.password);
+    this.email = this.password = '';
+  }
 
-ngOnInit () {
-
-}
+  logout() {
+    this.authService.logout();
+  }
 }
--- skyline/src/app/app.module.ts
+++ skyline/src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HttpModule } from '@angular/http';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireModule} from 'angularfire2/angularfire2';
+import { AuthService } from './providers/auth.service';
+
+  // Initialize Firebase
+  export const firebaseConfig = {
+    apiKey: 'AIzaSyCqP2gz1bPSBAotKR-Ohq306fPwC_3btW4',
+    authDomain: 'hello-world-196112.firebaseapp.com',
+    databaseURL: 'https://hello-world-196112.firebaseio.com',
+    projectId: 'hello-world-196112',
+    storageBucket: 'hello-world-196112.appspot.com',
+    messagingSenderId: '881736318085'
+  };
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
+    HeaderComponent
   ],
 
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule.enablePersistence()
+   ],
 
-  providers: [],
+  providers: [AuthService],
 
   bootstrap: [AppComponent]
 })
