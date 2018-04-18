cbeeb16ee5a3922ae3fd02d3c442734901c28d4b
fabio leotta <fabioleotta@yahoo.it>
2018-04-18 21:11:30 +0200

    l

--- src/app/card-hover.directive.ts
+++ src/app/card-hover.directive.ts
@@ -1,19 +1,26 @@
-import { Directive, Renderer, ElementRef, HostListener } from '@angular/core';
+import { Directive, Renderer, ElementRef, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[appCardHover]'
 })
 export class CardHoverDirective {
-
+  @Input('appCardHover') config:  {
+    querySelector: ''
+  };
+  @HostBinding('class.card-outline-primary') private ishovering:boolean;
+ 
   constructor(private el: ElementRef, private renderer: Renderer) {
    // renderer.setElementStyle(el.nativeElement, 'background-color', 'gray');
    }
+  
    @HostListener('mouseover') onmouseover(){
-     let part = this.el.nativeElement.querySelector('.card-text');
+     let part = this.el.nativeElement.querySelector(this.config.querySelector);
      this.renderer.setElementStyle(part,'display', 'block');
+     this.ishovering=true;
    }
    @HostListener('mouseout') onmouseout(){
-    let part = this.el.nativeElement.querySelector('.card-text');
+    let part = this.el.nativeElement.querySelector(this.config.querySelector);
     this.renderer.setElementStyle(part,'display', 'none');
+    this.ishovering=false;
   }
 }
