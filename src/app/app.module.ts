7e48f5dd91d9c761e06a78afcdd9e65c9de016ab
fabio leotta <fabioleotta@yahoo.it>
2018-04-18 12:34:06 +0200

    directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { JokeComponent } from './joke/joke.component';
 import { JokeListComponent } from './joke-list/joke-list.component';
 import { JokeFormComponent } from './joke-form/joke-form.component';
+import { CardHoverDirective } from './card-hover.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     JokeComponent,
     JokeListComponent,
-    JokeFormComponent
+    JokeFormComponent,
+    CardHoverDirective
   ],
   imports: [
     BrowserModule,
