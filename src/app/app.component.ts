d203a98de7276ca7058c28c909afee87dd65e829
fabio leotta <fabioleotta@yahoo.it>
2018-04-26 11:03:34 +0200

    router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component,   } from '@angular/core';
+import { Component   } from '@angular/core';
 
-import { SearchService } from './search-service';
+
 
 
 @Component({
@@ -10,13 +10,5 @@ import { SearchService } from './search-service';
 
 })
 export class AppComponent {
-  private loading: boolean=false;
-constructor(private itunes: SearchService){
-
-}
-doSearch(term: string){
-  this.loading=true;
-  this.itunes.search(term).then(_ =>this.loading=false)
-}
-
+ 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule, Http } from '@angular/http';
-
+import { NgModule, Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule, Http, JsonpModule } from '@angular/http';
+import {Routes, RouterModule, RouterLink} from '@angular/router';
 import {SearchService} from './search-service';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
+import { HomeComponent } from './home/home.component';
+import { ArtistComponent } from './artist/artist.component';
+import { ArtistTrackListComponent } from './artist-track-list/artist-track-list.component';
+import { ArtistAlbumListComponent } from './artist-album-list/artist-album-list.component';
 
+
+const routes: Routes=[
+  {path:'', redirectTo:'home', pathMatch:'full'},
+  {path:'find', redirectTo:'search', pathMatch:'full'},
+  {path:'home',component:HomeComponent},
+  {path:'search', component:SearchComponent},
+  {path:'artist/:artistId', component:ArtistComponent,
+children:[{path:'', redirectTo:'tracks', pathMatch:'full'},
+{path:'tracks', component:ArtistTrackListComponent},
+{path:'albums', component:ArtistAlbumListComponent},
+]
+  },
+  {path:'**', component:HomeComponent},
+];
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    HomeComponent,
+    ArtistComponent,
+    ArtistTrackListComponent,
+    ArtistAlbumListComponent,
  
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    JsonpModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes, {useHash:true})
   ],
   providers: [SearchService],
   bootstrap: [AppComponent]
